8afd65c5fea3d3f72b27ec91aa6c65f12eb3f930
Byishimo Cedrick <byishimocedrick@gmail.com>
2023-07-19 12:40:16 +0200

    first commit

---
+++ greatest-common-divisor.js
@@ -0,0 +1,16 @@
+let a = 24;
+let b = 18;
+
+let small = null;
+let Divsi
+
+a <= b ? small = a : small = b;
+    
+for(let x = 1; x <= small; x++){
+    if(a % x === 0 && b % x === 0){
+        Divsi = x
+    
+    }
+    
+}
+console.log(Divsi)
